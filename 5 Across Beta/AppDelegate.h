55eaffa5aed3127fce0efcb7ff4054e3419a5054
Kyle Raney <raney24fcb@gmail.com>
2015-04-17 17:02:46 -0400

    Initial Commit

---
+++ 5 Across Beta/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  5 Across Beta
+//
+//  Created by Kyle Raney on 4/17/15.
+//  Copyright (c) 2015 Kyle Raney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 5 Across Beta/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  5 Across Beta
+//
+//  Created by Kyle Raney on 4/17/15.
+//  Copyright (c) 2015 Kyle Raney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
